8b63c543326ac1ef3a1d97a937091a0c5da4ccc8
DE ZORDO Benjamin <bdezordo@gmail.com>
2023-12-13 13:30:26 +0100

    Visitor

---
+++ RobotScriptExtension/src/semantics/prof.ts
@@ -0,0 +1,82 @@
+
+export class comp implements RoboScriptVisitor{
+    visitAnyType(node: AnyType) {
+        throw new Error('Method not implemented.');
+    }
+    visitAssign(node: Assign) {
+        throw new Error('Method not implemented.');
+    }
+    visitAssignVar(node: AssignVar) {
+        throw new Error('Method not implemented.');
+    }
+    visitBinExpr(node: BinExpr) {
+        throw new Error('Method not implemented.');
+    }
+    visitBlock(node: Block) {
+        throw new Error('Method not implemented.');
+    }
+    visitDistance(node: Distance) {
+        throw new Error('Method not implemented.');
+    }
+    visitEntryPoint(node: EntryPoint) {
+        throw new Error('Method not implemented.');
+    }
+    visitExpression(node: Expression) {
+        throw new Error('Method not implemented.');
+    }
+    visitFunCall(node: FunCall) {
+        throw new Error('Method not implemented.');
+    }
+    visitFunDef(node: FunDef) {
+        throw new Error('Method not implemented.');
+    }
+    visitGetSpeed(node: GetSpeed) {
+        throw new Error('Method not implemented.');
+    }
+    visitIfStmt(node: IfStmt) {
+        throw new Error('Method not implemented.');
+    }
+    visitLinear(node: Linear) {
+        throw new Error('Method not implemented.');
+    }
+    visitLit(node: Lit) {
+        throw new Error('Method not implemented.');
+    }
+    visitRef(node: Ref) {
+        throw new Error('Method not implemented.');
+    }
+    visitReturnStmt(node: ReturnStmt) {
+        throw new Error('Method not implemented.');
+    }
+    visitRotation(node: Rotation) {
+        throw new Error('Method not implemented.');
+    }
+    visitSensor(node: Sensor) {
+        throw new Error('Method not implemented.');
+    }
+    visitSetSpeed(node: SetSpeed) {
+        throw new Error('Method not implemented.');
+    }
+    visitSpeed(node: Speed) {
+        throw new Error('Method not implemented.');
+    }
+    visitStatement(node: Statement) {
+        throw new Error('Method not implemented.');
+    }
+    visitTime(node: Time) {
+        throw new Error('Method not implemented.');
+    }
+    visitUnExpr(node: UnExpr) {
+        throw new Error('Method not implemented.');
+    }
+    visitUnitCast(node: UnitCast) {
+        throw new Error('Method not implemented.');
+    }
+    visitVarDecl(node: VarDecl) {
+        throw new Error('Method not implemented.');
+    }
+    visitWhileStmt(node: WhileStmt) {
+        throw new Error('Method not implemented.');
+    }
+
+}
--- RobotScriptExtension/src/semantics/visitor.ts
+++ RobotScriptExtension/src/semantics/visitor.ts
@@ -1,14 +1,355 @@
-import * as ASTInterfaces from '../language/generated/ast.js';
-import { Reference } from 'langium';
+import { CstNode, LangiumDocument, AstNode, Reference } from 'langium';
+import * as ASTInterfaces from '../language/generated/ast.js'
+import {isAssignVar, isSetSpeed } from '../language/generated/ast.js'
 
-export interface RobotScriptVisitor{
-    // TODO : create one visit method for each concept of the language
-    visitConcept(node : Concept) : any;
+//import { Reference } from 'langium';
+
+export interface RoboScriptVisitor {
+    visitAnyType(node: AnyType): any;
+    // visitAssign(node: Assign): any;
+    visitAssignVar(node: AssignVar): any;
+    visitBinExpr(node: BinExpr): any;
+    visitBlock(node: Block): any;
+    visitDistance(node: Distance): any;
+    visitEntryPoint(node: EntryPoint): any;
+    // visitExpression(node: Expression): any;
+    visitFunCall(node: FunCall): any;
+    visitFunDef(node: FunDef): any;
+    visitGetSpeed(node: GetSpeed): any;
+    visitIfStmt(node: IfStmt): any;
+    visitLinear(node: Linear): any;
+    visitLit(node: Lit): any;
+    visitRef(node: Ref): any;
+    visitReturnStmt(node: ReturnStmt): any;
+    visitRotation(node: Rotation): any;
+    visitSensor(node: Sensor): any;
+    visitSetSpeed(node: SetSpeed): any;
+    visitSpeed(node: Speed): any;
+    visitStatement(node: Statement): any;
+    visitTime(node: Time): any;
+    visitUnExpr(node: UnExpr): any;
+    visitUnitCast(node: UnitCast): any;
+    visitVarDecl(node: VarDecl): any;
+    visitWhileStmt(node: WhileStmt): any;
 }
 
-// TODO : create one concrete class for each concept
-export class Concept implements ASTInterfaces.Concept {
-    // the constructor must take all attribute of the implemented interface 
-    constructor(public $type: 'Type'){}
-    accept (visitor: RobotScriptVisitor) : any {}
+
+/** 
+export class EntryPointVisitor implements ASTInterfaces.EntryPoint {
+    // the constructor must take all attribute of the implemented interface
+    constructor(public $type: 'EntryPoint') {}
+    funs: ASTInterfaces.FunDef[] = []
+    accept(visitor: RoboScriptVisitor): any {
+        console.log('EntryPointVisitor')
+        visitor.visitEntryPoint(this);
+        this.funs.forEach(fun => fun.accept(visitor));
+    }
 }
+*/
+
+export class AnyType implements ASTInterfaces.AnyType {
+    constructor(public $type: 'AnyType') {
+    }
+    $container!: ASTInterfaces.FunDef | ASTInterfaces.VarDecl;
+    name!: 'number' | 'bool' | 'void';
+    $containerProperty?: string | undefined;
+    $containerIndex?: number | undefined;
+    $cstNode?: CstNode | undefined;
+    $document?: LangiumDocument<AstNode> | undefined;
+
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitAnyType(this);
+    }
+}
+
+
+// AssignVar : Block | IfStmt | WhileStmt
+export class AssignVar implements ASTInterfaces.AssignVar {
+    constructor(public $type: 'AssignVar') {}
+    $container!: ASTInterfaces.IfStmt | ASTInterfaces.Block | ASTInterfaces.WhileStmt;
+    $containerProperty?: string | undefined;
+    $containerIndex?: number | undefined;
+    $cstNode?: CstNode | undefined;
+    $document?: LangiumDocument<AstNode> | undefined;
+    expr!: ASTInterfaces.Expression;
+    ref!: Reference<ASTInterfaces.VarDecl>;
+
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitAssignVar(this);
+    }
+}
+
+// BinExpr : AssignVar | BinExpr | FunCall | IfStmt | Linear | ReturnStmt | Rotation | SetSpeed | UnExpr | VarDecl | WhileStmt
+export class BinExpr implements ASTInterfaces.BinExpr {
+    constructor(public $type: 'BinExpr') {}
+    $container!: ASTInterfaces.AssignVar | ASTInterfaces.BinExpr | ASTInterfaces.FunCall | ASTInterfaces.IfStmt | ASTInterfaces.Linear | ASTInterfaces.ReturnStmt | ASTInterfaces.Rotation | ASTInterfaces.SetSpeed | ASTInterfaces.UnExpr | ASTInterfaces.VarDecl | ASTInterfaces.WhileStmt;
+    expr1!: ASTInterfaces.Expression
+    expr2!: ASTInterfaces.Expression
+    op!: '!=' | '&&' | '*' | '+' | '-' | '/' | '<' | '<=' | '==' | '>' | '>=' | '||'
+
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitBinExpr(this);
+    }
+}
+
+
+// Block
+export class Block implements ASTInterfaces.Block {
+    constructor(public $type: 'Block') {}
+    $container!: ASTInterfaces.FunDef | ASTInterfaces.IfStmt | ASTInterfaces.Block | ASTInterfaces.WhileStmt;
+    stmts!: ASTInterfaces.Statement[];
+    $containerProperty?: string | undefined;
+    $containerIndex?: number | undefined;
+    $cstNode?: CstNode | undefined;
+    $document?: LangiumDocument<AstNode> | undefined;
+    type!: 'Block';
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitBlock(this);
+    }
+}
+
+// Distance
+export class Distance implements ASTInterfaces.Distance {
+    constructor(public $type: 'Distance') {}
+    val!: 'Distance';
+    $container?: AstNode | undefined;
+    $containerProperty?: string | undefined;
+    $containerIndex?: number | undefined;
+    $cstNode?: CstNode | undefined;
+    $document?: LangiumDocument<AstNode> | undefined;
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitDistance(this);
+    }
+}
+
+// EntryPoint
+export class EntryPoint implements ASTInterfaces.EntryPoint {
+    constructor(public $type: 'EntryPoint') {}
+    funs!: ASTInterfaces.FunDef[];
+    $container?: AstNode | undefined;
+    $containerProperty?: string | undefined;
+    $containerIndex?: number | undefined;
+    $cstNode?: CstNode | undefined;
+    $document?: LangiumDocument<AstNode> | undefined;
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitEntryPoint(this);
+    }
+}
+
+
+
+// FunCall
+export class FunCall implements ASTInterfaces.FunCall {
+    constructor(public $type: 'FunCall') {}
+    $container!: ASTInterfaces.AssignVar | ASTInterfaces.BinExpr | ASTInterfaces.Block | ASTInterfaces.FunCall | ASTInterfaces.IfStmt | ASTInterfaces.Linear | ASTInterfaces.ReturnStmt | ASTInterfaces.Rotation | ASTInterfaces.SetSpeed | ASTInterfaces.UnExpr | ASTInterfaces.VarDecl | ASTInterfaces.WhileStmt;
+    fun!: Reference<ASTInterfaces.FunDef>
+    params!: Array<ASTInterfaces.Expression>
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitFunCall(this);
+    }
+}
+
+
+//FunDef
+export class FunDef implements ASTInterfaces.FunDef {
+    constructor(public $type: 'FunDef') {}
+    $container!: ASTInterfaces.EntryPoint;
+    body!: ASTInterfaces.Block;
+    name!: string;
+    params!: ASTInterfaces.VarDecl[];
+    type!: ASTInterfaces.AnyType;
+    $containerProperty?: string | undefined;
+    $containerIndex?: number | undefined;
+    $cstNode?: CstNode | undefined;
+    $document?: LangiumDocument<AstNode> | undefined;
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitFunDef(this);
+    }
+}
+
+// GetSpeed
+export class GetSpeed implements ASTInterfaces.GetSpeed {
+    constructor(public $type: 'GetSpeed') {}
+    unit!: ASTInterfaces.Unit;
+    $container?: AstNode | undefined;
+    $containerProperty?: string | undefined;
+    $containerIndex?: number | undefined;
+    $cstNode?: CstNode | undefined;
+    $document?: LangiumDocument<AstNode> | undefined;
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitGetSpeed(this);
+    }
+}
+
+// IfStmt
+export class IfStmt implements ASTInterfaces.IfStmt {
+    constructor(public $type: 'IfStmt') {}
+    $container!: ASTInterfaces.Block | ASTInterfaces.IfStmt | ASTInterfaces.WhileStmt;
+    expr!: ASTInterfaces.Expression;
+    stmt1!: ASTInterfaces.Statement;
+    stmt2?: ASTInterfaces.Statement | undefined;
+    $containerProperty?: string | undefined;
+    $containerIndex?: number | undefined;
+    $cstNode?: CstNode | undefined;
+    $document?: LangiumDocument<AstNode> | undefined;
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitIfStmt(this);
+    }
+}
+
+// Linear
+export class Linear implements ASTInterfaces.Linear {
+    constructor(public $type: 'Linear') {}
+    dir!: 'Forward' | 'Sideways';
+    expr!: ASTInterfaces.Expression;
+    $container?: AstNode | undefined;
+    $containerProperty?: string | undefined;
+    $containerIndex?: number | undefined;
+    $cstNode?: CstNode | undefined;
+    $document?: LangiumDocument<AstNode> | undefined;
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitLinear(this);
+    }
+}
+
+
+// Lit
+export class Lit implements ASTInterfaces.Lit {
+    constructor(public $type: 'Lit') {}
+    $container?: AssignVar | BinExpr | FunCall | IfStmt | Linear | ReturnStmt | Rotation | SetSpeed | UnExpr | VarDecl | WhileStmt;
+    $type: 'Lit';
+    val!: boolean | number
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitLit(this);
+    }
+}
+
+
+// Ref
+export class Ref implements ASTInterfaces.Ref {
+    constructor(public $type: 'Ref') {}
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitRef(this);
+    }
+}
+
+
+// ReturnStmt
+export class ReturnStmt implements ASTInterfaces.ReturnStmt {
+    constructor(public $type: 'ReturnStmt') {}
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitReturnStmt(this);
+    }
+}
+
+
+
+// Rotation
+export class Rotation implements ASTInterfaces.Rotation {
+    constructor(public $type: 'Rotation') {}
+    $container!: ASTInterfaces.Block | ASTInterfaces.IfStmt | ASTInterfaces.WhileStmt;
+    expr!: ASTInterfaces.Expression;
+    $containerProperty?: string | undefined;
+    $containerIndex?: number | undefined;
+    $cstNode?: CstNode | undefined;
+    $document?: LangiumDocument<AstNode> | undefined;
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitRotation(this);
+    }
+}
+
+
+
+
+// SetSpeed
+export class SetSpeed implements ASTInterfaces.SetSpeed {
+    constructor(public $type: 'SetSpeed') {}
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitSetSpeed(this);
+    }
+}
+
+
+// Speed
+export class Speed implements ASTInterfaces.Speed {
+    constructor(public $type: 'Speed') {}
+    val!: 'Speed';
+    unit!: ASTInterfaces.Unit;
+    $container?: AstNode | undefined;
+    $containerProperty?: string | undefined;
+    $containerIndex?: number | undefined;
+    $cstNode?: CstNode | undefined;
+    $document?: LangiumDocument<AstNode> | undefined;
+    expr!: ASTInterfaces.Expression;
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitSpeed(this);
+    }
+}
+
+
+// Time
+export class Time implements ASTInterfaces.Time {
+    constructor(public $type: 'Time') {}
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitTime(this);
+    }
+}
+
+// UnExpr
+export class UnExpr implements ASTInterfaces.UnExpr {
+    constructor(public $type: 'UnExpr') {}
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitUnExpr(this);
+    }
+}
+
+// UnitCast
+export class UnitCast implements ASTInterfaces.UnitCast {
+    constructor(public $type: 'UnitCast') {}
+    unit!: ASTInterfaces.Unit;
+    val!: 'Distance';
+    $container?: AstNode | undefined;
+    $containerProperty?: string | undefined;
+    $containerIndex?: number | undefined;
+    $cstNode?: CstNode | undefined;
+    $document?: LangiumDocument<AstNode> | undefined;
+    dir!: 'Forward' | 'Sideways';
+    expr!: ASTInterfaces.Expression;
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitUnitCast(this);
+    }
+}
+
+// VarDecl
+export class VarDecl implements ASTInterfaces.VarDecl {
+    constructor(public $type: 'VarDecl') {}
+    $container!: ASTInterfaces.Block | ASTInterfaces.IfStmt | ASTInterfaces.WhileStmt | ASTInterfaces.FunDef;
+    expr?: ASTInterfaces.Expression | undefined;
+    name!: string;
+    type!: ASTInterfaces.AnyType;
+    $containerProperty?: string | undefined;
+    $containerIndex?: number | undefined;
+    $cstNode?: CstNode | undefined;
+    $document?: LangiumDocument<AstNode> | undefined;
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitVarDecl(this);
+    }
+}
+
+
+// WhileStmt
+export class WhileStmt implements ASTInterfaces.WhileStmt {
+    constructor(public $type: 'WhileStmt') {}
+    $container!: ASTInterfaces.Block | ASTInterfaces.IfStmt | ASTInterfaces.WhileStmt;
+    expr!: ASTInterfaces.Expression;
+    stmt!: ASTInterfaces.Statement;
+    $containerProperty?: string | undefined;
+    $containerIndex?: number | undefined;
+    $cstNode?: CstNode | undefined;
+    $document?: LangiumDocument<AstNode> | undefined;
+    accept(visitor: RoboScriptVisitor): any {
+        return visitor.visitWhileStmt(this);
+    }
+}
+
+
